4cbdeec0349dfc8b6ec465e83204c65b3aa76183
Mohit Purohit <mohitpurohit020@gmail.com>
2025-11-23 13:31:21 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Root Layout Page</h1>;
+  return (
+    <>
+      
+    </>
+  )
 }
---
+++ app/poke/page.js
@@ -0,0 +1,13 @@
+'use client'
+import React, { useState, useEffect } from 'react'
+
+const Poke = () => {
+   
+    return (
+    <>
+        <h1>Pokemon page</h1>
+    </>
+  )
+}
+
+export default Poke;
